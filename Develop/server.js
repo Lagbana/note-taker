968afc99e25b1e0685c5514e33999c7ce1f16fa3
Larry_A <larryagbana@gmail.com>
2020-04-24 12:10:07 -0400

    functioning public routes

--- Develop/server.js
+++ Develop/server.js
@@ -2,100 +2,33 @@
 // ==============================================
 const express = require('express')
 const path = require('path')
+const fs = require('fs').promises
+const { v4 } = require('uuid')
+const uniqueID = v4
+
 
 // Express App
 // ==============================================
 const app = express()
 const PORT = 3000
 
-// Sets up the Express app to handle data parsing
+// Set up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static(__dirname + '/public'))
 
+// Database file and folder
+const dbDir = path.resolve(__dirname, "./db");
+const dbpath = path.join(dbDir, 'db.json')
 
-const sampleNotes = [
-    {   
-        weekday: 'Monday',
-        note: 'Get shit done'
-    },
-    {   
-        weekday: 'Tuesday',
-        note: 'Get shit done'
-    },
-    {   
-        weekday: 'Wednesday',
-        note: 'Get shit done'
-    },
-    {   
-        weekday: 'Thursday',
-        note: 'Get shit done'
-    },
-    {   
-        weekday: 'Friday',
-        note: 'Get shit done'
-    },
-    {   
-        weekday: 'Saturday',
-        note: 'Get shit done'
-    },
-    {   
-        weekday: 'Sunday',
-        note: 'Get shit done'
-    }
-
-]
-
-// Route Handler for html pages
+// public route handling
 // =========================================================
-const publicRoutes = {index:'/', notes: '/notes'}
-const files = ['public/index.html', 'public/notes.html']
-
-const requestHandler = (path) => {
-    if (Object.values(publicRoutes).indexOf(path) === -1){
-        app.get('/', (req, res) => {
-            res.sendFile(path.join(__dirname, 'public/index.html'))
-        })
-    } else if (path === '/') {
-        app.get(path, (req, res) => {
-            res.sendFile(path.join(__dirname, files[0]))
-        })
-    } else if (path === '/notes') {
-        app.get(path, (req, res) => {
-            res.sendFile(path.join(__dirname, files[1]))
-        })
-    }
-}
-requestHandler()
-
-// requestHandler('/', 'public/index.html')
-// requestHandler('/notes', 'public/notes.html')
-
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public/index.html'))
-// })
-// app.get('/notes', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public/notes.html'))
-// })
-
-// Displays all the notes
-app.get('/api/notes', (req, res) => {
-    return res.json(sampleNotes)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 })
-
-// Get a specific note
-app.get('/api/notes/:slot', (req, res) => {
-    const slot = req.params.slot
-
-    for (const note of sampleNotes) {
-
-        if (slot.toLowerCase() === note.weekday.toLowerCase()) {
-            return res.json(note)
-        }
-    }
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 })
-
 
 // Server listening
 // ====================================================================
